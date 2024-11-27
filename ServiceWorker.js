b1b2b0abb4cf998b3ab68629bed3b4cda0a590c5
Reza Siamian <rezasg78@gmail.com>
2024-11-27 14:22:30 +0330

    no message

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Skillus-Dev24Aban-1.12";
+const contentToCache = [
+    "Build/web 7 azar.loader.js",
+    "Build/web 7 azar.framework.js",
+    "Build/web 7 azar.data",
+    "Build/web 7 azar.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
